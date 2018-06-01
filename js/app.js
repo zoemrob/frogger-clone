e017f2ed6a77ddf63f39a6080dc32efb7e63d3f3
Zoe Robertson <zoe.robertson@getalma.com>
2018-05-31 17:56:37 -0700

    feat: added character selection screen
    on load, character selection is presented, when a character is selected
    and the Start button is clicked, the Engine loads with selected character.

--- js/app.js
+++ js/app.js
@@ -1,46 +1,176 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+// // Enemies our player must avoid
+// const Enemy = function() {
+//     // Variables applied to each of our instances go here,
+//     // we've provided one for you to get started
+//
+//     // The image/sprite for our enemies, this uses
+//     // a helper we've provided to easily load images
+//     this.sprite = 'images/enemy-bug.png';
+// };
+//
+// // Update the enemy's position, required method for game
+// // Parameter: dt, a time delta between ticks
+// Enemy.prototype.update = function(dt) {
+//     // You should multiply any movement by the dt parameter
+//     // which will ensure the game runs at the same speed for
+//     // all computers.
+// };
+//
+// // Draw the enemy on the screen, required method for game
+// Enemy.prototype.render = function() {
+//     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+// };
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
+class Enemy {
+    constructor () {
+        this.x = 400;
+        this.y = 150;
+        this.sprite = 'images/enemy-bug.png';
+    }
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
+    update (dt) {
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+    }
+
+    render () {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+}
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+// const Player = function () {
+//     this.sprite = 'images/char-boy.png';
+// };
+//
+// Player.prototype.render = function() {
+//     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+// };
+//
+// Player.prototype.update = function () {
+//     this.handleInput();
+// };
+//
+// Player.prototype.handleInput = function (key) {
+//     if (key !== undefined) {
+//
+//     }
+// };
 
+class Player {
+    constructor () {
+        this.sprite = sessionStorage.getItem('char');
+        this.x = 203;
+        this.y = 403;
+    }
+
+    render () {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+
+    update () {
+
+    }
+
+    handleInput (key) {
+        if (typeof key === "undefined") {
+
+        }
+    }
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+const allEnemies = [new Enemy(), new Enemy()];
+const player = new Player();
 
+// character select screen
+const gameInit = () => {
+    const charTable = document.getElementById('js-char-select'),
+        startButton = document.getElementById('js-new-game'),
+        imgMap = {
+            "js-char-1": "images/char-boy.png",
+            "js-char-2": "images/char-cat-girl.png",
+            "js-char-3": "images/char-horn-girl.png",
+            "js-char-4": "images/char-pink-girl.png",
+            "js-char-5": "images/char-princess-girl.png"
+        };
+    let selectedId = undefined;
 
+    // set default if no character is selected.
+    sessionStorage.setItem('char', 'images/char-boy.png');
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
+    charTable.addEventListener('click', charSelectClick);
 
-    player.handleInput(allowedKeys[e.keyCode]);
-});
+    function charSelectClick (e) {
+        selectedId = undefined;
+        const prevSelected = document.querySelector('td.selected');
+        // remove highlight from previous selection
+        if (prevSelected !== null) {
+            prevSelected.classList.remove('selected');
+        }
+
+        if (e.target.nodeName === 'IMG') {
+            selectedId = e.target.getAttribute('id');
+        }
+
+        if (typeof selectedId !== "undefined") {
+            e.target.parentElement.classList.add('selected');
+            sessionStorage.setItem('char', imgMap[selectedId]);
+        }
+    }
+
+    document.addEventListener('keydown', charSelectKey);
+
+    function charSelectKey (e) {
+        selectedId = undefined;
+        const prevSelected = document.querySelector('td.selected');
+        let next = undefined;
+        if (e.key === 'ArrowRight') {
+            console.log(prevSelected);
+            next = prevSelected.nextElementSibling;
+            console.log(next);
+            if (next === null) {
+                next = prevSelected.parentElement.firstElementChild;
+                console.log(next);
+            }
+        } else if (e.key === 'ArrowLeft') {
+            next = prevSelected.previousElementSibling;
+            if (next === null) {
+                next = prevSelected.parentElement.lastElementChild;
+            }
+        }
+
+        if (typeof next !== "undefined") {
+            // remove highlight from previous selection
+            if (prevSelected !== null) {
+                prevSelected.classList.remove('selected');
+            }
+            selectedId = next.firstElementChild.getAttribute('id');
+            next.classList.add('selected');
+            sessionStorage.setItem('char', imgMap[selectedId]);
+        }
+    }
+
+    startButton.addEventListener('click', () => {
+        const menu = document.querySelector('main');
+        menu.classList.toggle('hidden');
+        // initialize board
+        Engine();
+        // This listens for key presses and sends the keys to your
+        // Player.handleInput() method. You don't need to modify this.
+        document.addEventListener('keyup', e => {
+            const allowedKeys = {
+                37: 'left',
+                38: 'up',
+                39: 'right',
+                40: 'down'
+            };
+
+            player.handleInput(allowedKeys[e.keyCode]);
+        });
+    });
+};
+document.addEventListener('DOMContentLoaded', gameInit);
--- js/engine.js
+++ js/engine.js
@@ -13,7 +13,7 @@
  * writing app.js a little simpler to work with.
  */
 
-var Engine = (function(global) {
+const Engine = function(global = this) {
     /* Predefine the variables we'll be using within this scope,
      * create the canvas element, grab the 2D context for that canvas
      * set the canvas elements height/width and add it to the DOM.
@@ -173,7 +173,11 @@ var Engine = (function(global) {
         'images/water-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
-        'images/char-boy.png'
+        'images/char-boy.png',
+        'images/char-cat-girl.png',
+        'images/char-horn-girl.png',
+        'images/char-pink-girl.png',
+        'images/char-princess-girl.png'
     ]);
     Resources.onReady(init);
 
@@ -182,4 +186,4 @@ var Engine = (function(global) {
      * from within their app.js files.
      */
     global.ctx = ctx;
-})(this);
+};
